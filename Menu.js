996bb41005436983a34dadce6e53dafd2225d344
Pavitrakandula <50817852+Pavitrakandula@users.noreply.github.com>
2021-01-04 19:00:24 +0530

    04-01-2021
    Switch Case

---
+++ Menu.js
@@ -0,0 +1,15 @@
+
+
+            function food(){
+        
+    var Menu = document.getElementById("Menu").value;
+
+      switch(Menu){
+
+        case "Chicken Majestic": document.getElementById("Indianfood").innerHTML="It has freshly cooked chicken with special spices cooked on a slow flame with seasonings."; break;
+        case "Butter Chicken": document.getElementById("Indianfood").innerHTML="Mix of indian spices with lots of butter and cream"; break;
+        case "Naan": document.getElementById("Indianfood").innerHTML="A bread made of all purpose flour, its made into a thin sheet form."; break;
+        case "Biryani": document.getElementById("Indianfood").innerHTML="Biryani is a mixed rice dish with its origins among the Muslims of the Indian subcontinent. It is made with Indian spices, rice, and meat, or vegetables and sometimes, in addition, eggs and/or potatoes in certain regional varieties. Biryani is popular throughout the Indian subcontinent, as well as among its diaspora."; break;
+        
+    }
+}
